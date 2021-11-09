c2aa07217d0aab7ec65e66f92b44707109b82787
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2021-11-09 14:15:16 +0530

    updated schema validations

--- models/Events.js
+++ models/Events.js
@@ -8,43 +8,30 @@ const EventSchema = mongoose.Schema({
   },
   Title: {
     type: String,
-    maxlength: 200,
-    minlength: 10,
     required: true,
   },
   Caption: {
     type: String,
-    minlength: 10,
-    maxlength: 600,
     required: true,
   },
   Description: {
     type: String,
-    minlength: 20,
     required: true,
-    maxlength: 1600,
   },
   OrganizingClub: {
     type: String,
-    minlength: 5,
-    maxlength: 75,
     required: true,
   },
   Date: {
     type: Date,
-    minlength: 5,
-    maxlength: 9,
     required: true,
   },
   Venue: {
     type: String,
-    minlength: 3,
-    maxlength: 50,
     required: true,
   },
   RegistrationLink: {
     type: String,
-    minlength: 5,
     required: true,
   },
   Note: {
